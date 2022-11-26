d8e6532f66d86a4bbc14241d61d12c08ecc20ca2
Aleksey Beketov <alex311271@gmail.com>
2022-11-26 11:14:14 +0300

    return to normal

--- src/test/java/PostmanEchoTest.java
+++ src/test/java/PostmanEchoTest.java
@@ -14,6 +14,6 @@ public class PostmanEchoTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Это автоматический тест"));
+                .body("data", equalTo("Это автоматический тест API"));
     }
 }
