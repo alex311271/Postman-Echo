aa8a73298c16fc7cc82e51d34a7b65860da3e44f
Aleksey Beketov <alex311271@gmail.com>
2022-11-26 10:54:51 +0300

    initioanal commit

---
+++ src/test/java/PostmanEchoTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class PostmanEchoTest {
+    @Test
+    public void postRequestTest() {
+        given()
+                .baseUri("https://postman-echo.com")
+                .contentType("text/plain; charset=UTF-8")
+                .body("Это автоматический тест API") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .when()
+                .post("/post")
+                .then()
+                .statusCode(200)
+                .body("data", equalTo("Это автоматический тест API"));
+    }
+}
